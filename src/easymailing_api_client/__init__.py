db2337e3fdc38a92b82c4107f2704b2732310047
Juan Luis Cano Rodríguez <hello@juanlu.space>
2023-05-28 22:01:01 +0200

    Migrate to flit

--- src/easymailing_api_client/__init__.py
+++ src/easymailing_api_client/__init__.py
@@ -1,5 +1,9 @@
-""" A client library for accessing Easymailing API """
+"""
+A client library for accessing Easymailing API
+"""
 from .client import AuthenticatedClient, Client
+
+__version__ = "0.1.dev0"
 
 __all__ = (
     "AuthenticatedClient",
