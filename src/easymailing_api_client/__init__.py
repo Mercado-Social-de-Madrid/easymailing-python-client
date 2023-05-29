75cf00e07d74e6ed12786c30e557219f0c97c925
Juan Luis Cano Rodríguez <hello@juanlu.space>
2023-05-29 02:17:13 +0200

    Bump version

--- src/easymailing_api_client/__init__.py
+++ src/easymailing_api_client/__init__.py
@@ -3,7 +3,7 @@ A client library for accessing Easymailing API
 """
 from .client import AuthenticatedClient, Client
 
-__version__ = "0.1.dev0"
+__version__ = "0.1.0"
 
 __all__ = (
     "AuthenticatedClient",
